e90ec481235a8807ab32dbb2c266589be663f3e7
alisers <povaralisa@gmail.com>
2022-02-15 17:34:00 +0200

    .

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Profile from 'Profile/Profile.js';
+
+export default function App(){
+    return (
+        <div>
+        <Profile
+        // username={}
+        // tag={}
+        // location={}
+        // avatar={}
+        // stats={}
+        />
+    </div>
+    )
+
+    
+}
+
